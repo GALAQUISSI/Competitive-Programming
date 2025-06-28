4466129c86c8715a0cab4e47e76ae690da7871b4
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:21:18 -0300

    Create 1759.cpp

---
+++ Beecrowd/1759.cpp
@@ -0,0 +1,16 @@
+// beecrowd 1759
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i= 0; i < n; i++){
+        if(i < n - 1){
+            printf("Ho ");
+        }else{
+        printf("Ho!\n");
+    }
+    }
+    return 0;
+}
