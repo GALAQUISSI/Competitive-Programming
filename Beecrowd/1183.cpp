988bb994d96c4beb6f77f4952b28a9a89436774a
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:11:53 -0300

    Create 1183.cpp

---
+++ Beecrowd/1183.cpp
@@ -0,0 +1,35 @@
+// beecrowd 1183
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double m[12][12];
+    char t;
+    double sum = 0.0;
+    int count = 0;
+
+    cin >> t;
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+                cin >> m[i][j];
+        }
+    }
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            if(j > i){
+                sum += m[i][j];
+                count++;
+            }
+        }
+    }
+    cout << fixed << setprecision(1);
+    if(t == 'M'){
+        cout << (sum/count) << endl;
+    }else{
+        cout << sum << endl;
+    }
+
+    return 0;
+}
