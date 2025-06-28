279926406605210c622ad5437f367f3f41bf680b
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:12:51 -0300

    Create 1182.cpp

---
+++ Beecrowd/1182.cpp
@@ -0,0 +1,32 @@
+// beecrowd 1182
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+        int n;
+        double m[12][12];
+        char t;
+        double sum = 0;
+            cin >> n;
+            cin >> t;
+            for(int i = 0; i < 12; i++){
+                for( int j = 0; j < 12; j++){
+                        cin >> m[i][j];
+                }
+            }
+            for(int i = 0; i < 12; i++){
+                for(int j = 0; j < 12; j++){
+                    if(j == n){
+                            sum += m[i][j];
+                        }
+                }
+            }
+            if(t == 'M'){
+                printf("%.1lf\n", sum/12.0);
+            }else{
+                printf("%.1lf\n", sum);
+            }
+    return 0;
+}
