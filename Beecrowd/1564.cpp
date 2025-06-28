c0276a71a6b91d1b2e3d6977c53a5f2a9efbe9fc
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:22:39 -0300

    Create 1564.cpp

---
+++ Beecrowd/1564.cpp
@@ -0,0 +1,15 @@
+// beecrowd 1564
+
+#include<stdio.h>
+
+int main(){
+    int r;
+    while(scanf("%d", &r) != EOF){
+        if(r == 0){
+            printf("vai ter copa!\n");
+        }else{
+            printf("vai ter duas!\n");
+        }
+    }
+    return 0;
+}
