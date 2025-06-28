b2be7f7ecc7a33fddb639d8b60a4a4e952e132b3
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:20:34 -0300

    Create 1172.cpp

---
+++ Beecrowd/1172.cpp
@@ -0,0 +1,21 @@
+// beecrowd 1172
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x[10];
+        for(int i = 0; i < 10; i++){
+            cin >> x[i];
+        }
+        for(int i = 0; i  < 10; i++){
+            if(x[i] <= 0){
+                x[i] = 1;
+            }
+        }
+        for(int i = 0; i < 10; i++){
+            printf("X[%d] = %d\n", i, x[i]);
+        }
+    return 0;
+}
