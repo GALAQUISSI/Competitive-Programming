718cadf8dc8b0f2c49faae2eb91f9c6d85483914
Daniel Vilano Soares <danielvsoares06@gmail.com>
2025-06-28 16:16:23 -0300

    Create 1177.cpp

---
+++ Beecrowd/1177.cpp
@@ -0,0 +1,15 @@
+// beecrowd 1177
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+
+   for(int i = 0; i < 1000; i++){
+        printf("N[%d] = %d\n", i, i % t);
+   }
+        return 0;
+}
